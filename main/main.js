9cd802e15f6ed47d24436d582d5f1b7da1975532
Dixiepip <boss.sadovskiy@mail.ru>
2023-04-15 12:50:41 +0300

    slider end

--- main/main.js
+++ main/main.js
@@ -16,7 +16,9 @@ const startSlider = () => {
     let position = 0;
 
     const checkSlider = () => {
-     if (activeSlider + 2 === sliderItems.length) {
+     if ((activeSlider + 2 === sliderItems.length && 
+        document.documentElement.offsetWidth > 560) || 
+        activeSlider === sliderItems.length) {
         btnNextSlider.style.display = 'none';
      }  else {
         btnNextSlider.style.display = '';
@@ -32,29 +34,39 @@ const startSlider = () => {
     checkSlider();
 
     const nextSlider = () => {
-        sliderItems[activeSlider].classList.remove('slider__item_active');
+        sliderItems[activeSlider]?.classList.remove('slider__item_active');
         position = -sliderItems[0].clientWidth * activeSlider;
         
         sliderList.style.transform = `translateX(${position}px)`;
         activeSlider += 1;
-        sliderItems[activeSlider].classList.add('slider__item_active');
-        checkSlider()
+        sliderItems[activeSlider]?.classList.add('slider__item_active');
+        checkSlider();
     };
     const prevSlider = () => {
-        sliderItems[activeSlider].classList.remove('slider__item_active');
+        sliderItems[activeSlider]?.classList.remove('slider__item_active');
         position = -sliderItems[0].clientWidth * (activeSlider - 2);
         
         sliderList.style.transform = `translateX(${position}px)`;
         activeSlider -= 1;
-        sliderItems[activeSlider].classList.add('slider__item_active');
+        sliderItems[activeSlider]?.classList.add('slider__item_active');
         checkSlider();
     };
     btnPrevSlider.addEventListener('click', prevSlider);
     btnNextSlider.addEventListener('click', nextSlider);
     
+
     window.addEventListener('resize', () => {
-        console.log(1);
-    })
+        if (activeSlider + 2 > sliderItems.length && 
+            document.documentElement.offsetWidth > 560) {
+                activeSlider = sliderItems.length - 2;
+                sliderItems[activeSlider]?.classList.add('slider__item_active');
+        }
+
+
+        position = -sliderItems[0].clientWidth * (activeSlider - 1);
+        sliderList.style.transform = `translateX(${position}px)`;
+        checkSlider();
+    });
 };
 
 const initSlider = () => {
